c41109be0ff0fa4cb7331837d0db7100e3eb46d6
James Kristoff <jskrist@gmail.com>
2022-07-14 22:19:03 -0400

    added abilites: - to dynamically load tile set and - to redraw at will

--- sketch.js
+++ sketch.js
@@ -1,34 +1,40 @@
 let tiles = [];
-const tileImages = [];
-
+let tileImages = [];
+let fileList = [];
 let grid = [];
+let seed = 8;
 
-const DIM = 31;
-
-function preload() {
-  // const path = 'rail';
-  // for (let i = 0; i < 7; i++) {
-  //   tileImages[i] = loadImage(`${path}/tile${i}.png`);
-  // }
-
-  const path = 'tiles/circuit-coding-train';
-  for (let i = 0; i < 13; i++) {
-    tileImages[i] = loadImage(`${path}/${i}.png`);
-  }
-}
-
-function removeDuplicatedTiles(tiles) {
-  const uniqueTilesMap = {};
-  for (const tile of tiles) {
-    const key = tile.edges.join(','); // ex: "ABB,BCB,BBA,AAA"
-    uniqueTilesMap[key] = tile;
-  }
-  return Object.values(uniqueTilesMap);
-}
+const DIM = 20;
 
 function setup() {
   createCanvas(400, 400);
-  //randomSeed(15);
+  startOver();
+  noLoop();
+  drawBackground();
+  const inputElement = document.getElementById("tileImages");
+  inputElement.addEventListener("change", loadSelectedImages, false);
+}
+
+function loadSelectedImages() {
+  noLoop();
+  tiles = [];
+  tileImages = [];
+  fileList = this.files;
+  for (let i = 0; i < fileList.length; i++) {
+    imagePath = './tiles/' + fileList[i].webkitRelativePath;
+    loadImage(imagePath, addToTileImages);
+  }
+}
+
+function addToTileImages(im) {
+  tileImages.push(im);
+  if(tileImages.length == fileList.length) {
+    asyncSetup();
+  }
+}
+
+function asyncSetup() {
+  randomSeed(0);
 
   // Loaded and created the tiles in preload()
   // process the edges of each image now
@@ -63,12 +69,9 @@ function setup() {
   }
   tiles = tempTiles;
 
-  // console.log(tiles.length);
-
   // Generate the adjacency rules based on edges
   for (let i = 0; i < tiles.length; i++) {
     const tile = tiles[i];
-    // console.log("tileIndex: " + i)
     tile.analyze(tiles);
   }
 
@@ -76,109 +79,36 @@ function setup() {
   // noLoop();
 }
 
+function removeDuplicatedTiles(tiles) {
+  const uniqueTilesMap = {};
+  for (const tile of tiles) {
+    const key = tile.edges.join(','); // ex: "ABB,BCB,BBA,AAA"
+    uniqueTilesMap[key] = tile;
+  }
+  return Object.values(uniqueTilesMap);
+}
+
 function startOver() {
+  randomSeed(seed);
+  grid = [];
   // Create cell for each spot on the grid
   for (let i = 0; i < DIM * DIM; i++) {
     grid[i] = new Cell(tiles.length);
   }
+  loop();
 }
-
-function intersection(setA, setB) {
-  const _intersection = new Set();
-  for (const elem of setB) {
-    if (setA.has(elem)) {
-      _intersection.add(elem);
-    }
-  }
-  return _intersection;
-}
-
-function showNeighbors(tile) {
-  drawBackground();
-  thisTile = tiles[tile];
-  w = width / DIM;
-  h = height / DIM;
-  // show this tile in the center
-  image(thisTile.img, 15*w, 15*h, w, h)
-  // top neighbors
-  for(let i = 0; i < thisTile.up.length; i++) {
-    image(tiles[thisTile.up[i]].img, 15*w, (14 - i) * h, w, h)
-  }
-  // right neighbors
-  for(let i = 0; i < thisTile.right.length; i++) {
-    image(tiles[thisTile.right[i]].img, (16+i)*w, 15 * h, w, h)
-  }
-  // bottom neighbors
-  for(let i = 0; i < thisTile.down.length; i++) {
-    image(tiles[thisTile.down[i]].img, 15*w, (16+i) * h, w, h)
-  }
-  // left neighbors
-  for(let i = 0; i < thisTile.left.length; i++) {
-    image(tiles[thisTile.left[i]].img, (14-i)*w, 15 * h, w, h)
-  }
-}
-
-
-function tmp() {
-  w = width / DIM;
-  h = height / DIM;
-  for(let i = 0; i < tiles.length; i++) {
-    image(tiles[i].img, (i % 25) * w, (20 + (i >= 25)) * h, w, h)
-  }
-}
-
-
-function combineOptions(optionsToCheck, directionToCheck, validOptions = new Set()) {
-  let tmpValidOptions = new Set();
-  for (let option of optionsToCheck) {
-    let valid = tiles[option][directionToCheck];
-    valid.forEach(x => {tmpValidOptions.add(x)})
-  }
-  if(validOptions.size > 0) {
-    return intersection(validOptions, tmpValidOptions);
-  }
-  else {
-    return tmpValidOptions;
-  }
-
-}
-
-
-function mousePressed() {
-  redraw();
-}
-
-function drawBackground() {
-  background(0);
-
-  const w = width / DIM;
-  const h = height / DIM;
-  for (let j = 0; j < DIM; j++) {
-    for (let i = 0; i < DIM; i++) {
-      let cell = grid[i + j * DIM];
-      if (cell.collapsed) {
-        let index = cell.options[0];
-        image(tiles[index].img, i * w, j * h, w, h);
-      } else {
-        noFill();
-        stroke(51);
-        rect(i * w, j * h, w, h);
-      }
-    }
-  }
-}
-
 
 function draw() {
   
   drawBackground();
+
   // Pick cell with least entropy
   let gridCopy = grid.slice();
   gridCopy = gridCopy.filter((a) => !a.collapsed);
-  // console.table(grid);
-  // console.table(gridCopy);
 
   if (gridCopy.length == 0) {
+    console.log('Image Complete');
+    noLoop();
     return;
   }
   gridCopy.sort((a, b) => {
@@ -196,19 +126,18 @@ function draw() {
 
   if (stopIndex > 0) gridCopy.splice(stopIndex);
   const cell = random(gridCopy);
+  if(cell === grid[135]) {
+    console.log('found it')
+  }
   cell.collapsed = true;
   const pick = random(cell.options);
-  if(cell.options.length < 1) {
-    console.log('With ' + cell.options.length + ' options, ' + pick + ' was chosen.')
-  }
   if (pick === undefined) {
-    // noLoop();
     startOver();
     return;
   }
   cell.options = [pick];
 
-  const nextGrid = [];
+  let nextGrid = [];
   for (let j = 0; j < DIM; j++) {
     for (let i = 0; i < DIM; i++) {
       let index = i + j * DIM;
@@ -248,3 +177,84 @@ function draw() {
 
   grid = nextGrid;
 }
+
+function drawBackground() {
+  background(0);
+
+  const w = width / DIM;
+  const h = height / DIM;
+  for (let j = 0; j < DIM; j++) {
+    for (let i = 0; i < DIM; i++) {
+      let cell = grid[i + j * DIM];
+      if (cell.collapsed) {
+        let index = cell.options[0];
+        image(tiles[index].img, i * w, j * h, w, h);
+      } else {
+        noFill();
+        stroke(51);
+        rect(i * w, j * h, w, h);
+      }
+    }
+  }
+}
+
+function combineOptions(optionsToCheck, directionToCheck, validOptions = new Set()) {
+  let tmpValidOptions = new Set();
+  for (let option of optionsToCheck) {
+    let valid = tiles[option][directionToCheck];
+    valid.forEach(x => {tmpValidOptions.add(x)})
+  }
+  if(validOptions.size > 0) {
+    return intersection(validOptions, tmpValidOptions);
+  }
+  else {
+    return tmpValidOptions;
+  }
+}
+
+function intersection(setA, setB) {
+  const _intersection = new Set();
+  for (const elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem);
+    }
+  }
+  return _intersection;
+}
+
+function showNeighbors(tile) {
+  drawBackground();
+  thisTile = tiles[tile];
+  w = width / DIM;
+  h = height / DIM;
+  // show this tile in the center
+  image(thisTile.img, 15*w, 15*h, w, h)
+  // top neighbors
+  for(let i = 0; i < thisTile.up.length; i++) {
+    image(tiles[thisTile.up[i]].img, 15*w, (14 - i) * h, w, h)
+  }
+  // right neighbors
+  for(let i = 0; i < thisTile.right.length; i++) {
+    image(tiles[thisTile.right[i]].img, (16+i)*w, 15 * h, w, h)
+  }
+  // bottom neighbors
+  for(let i = 0; i < thisTile.down.length; i++) {
+    image(tiles[thisTile.down[i]].img, 15*w, (16+i) * h, w, h)
+  }
+  // left neighbors
+  for(let i = 0; i < thisTile.left.length; i++) {
+    image(tiles[thisTile.left[i]].img, (14-i)*w, 15 * h, w, h)
+  }
+}
+
+function showAllTiles() {
+  w = width / DIM;
+  h = height / DIM;
+  for(let i = 0; i < tiles.length; i++) {
+    image(tiles[i].img, (i % 25) * w, (20 + (i >= 25)) * h, w, h)
+  }
+}
+
+// function mousePressed() {
+//   redraw();
+// }
